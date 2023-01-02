78b45bd341256ca5b126d5c2cc21f542b8b9f313
Ganesh <ganesh.kumar@dsmartsys.in>
2023-01-02 12:23:33 +0530

    add_files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'p1';
+  constructor(private router: Router, private formBuilder: FormBuilder,
+    private http: HttpClient) { }
+    aboutDetails = this.formBuilder.group({
+      to_email: new FormControl('', Validators.required),
+      _subject: new FormControl(),
+      _textarea: new FormControl(),
+    })
+sendemail()
+{
+const popnotifications = {
+to_email: this.aboutDetails.value.to_email,
+_subject: this.aboutDetails.value._subject,
+_textarea: this.aboutDetails.value._textarea,
+};
+if(this.aboutDetails.invalid){
+  alert("Invalid/Missing Credentials");
+  this.aboutDetails.markAllAsTouched();
+ 
+}
+else
+{
+  this.http
+  
+  .post('http://localhost:3000/postsendemail',popnotifications)
+
+  .subscribe((res: any) => {
+    console.log("hai",res);
+    alert("send successfully");
+    this.clearform();
+})
+}
+}
+clearform()
+{
+  this.aboutDetails.reset();
+}
 }
